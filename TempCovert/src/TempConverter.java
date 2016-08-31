6108a56ed4a2fe4bf5e587c9513bfcebcf79d1cc
TheEricLam <18lame45@millshighsch.org>
2016-08-31 10:44:32 -0700

    Calculate Libary project

---
+++ TempCovert/src/TempConverter.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+
+/**
+ * @author Eric Lam
+ * @version 1.0
+ *
+ */
+public class TempConverter {
+	public static void main(String[] args) {
+		double c = 12; // C is for Celsius
+		double f = c * 9/5 + 32;
+		System.out.print("When it is " + c + " Celsius, it is ");
+		System.out.print(f + " fahrenheit.");
+	}
+
+}

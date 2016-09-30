d1dd3b4177ac22f117ef36731c3e11d1a60b60d9
TheEricLam <18lame45@millshighsch.org>
2016-09-30 09:08:45 -0700

    Test3

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/91/5081e90c288700161e9eed426bbfcf05
@@ -0,0 +1,11 @@
+// [Eric Lam] [2nd Period] [Version 1.0]
+
+import java.util.*;
+
+public class Processor {
+
+	public static void main(String[] args) {
+		Scanner userInput = new Scanner(System.in);
+	}
+
+}
--- ProcessingNumbers/src/Processor.java
+++ ProcessingNumbers/src/Processor.java
@@ -6,6 +6,18 @@ public class Processor {
 
 	public static void main(String[] args) {
 		Scanner userInput = new Scanner(System.in);
+		
+		int inputNumber;
+		int totalSum = 0;
+		
+		System.out.println("Enter 16 Integers: ");
+		
+		for (int i=1; i <= 16; i++){
+			inputNumber = userInputNumber.nextInt
+			if(inputNumber%2 == 0){
+				System.out.println(inputNumber);
+			}
+		}
 	}
 
 }

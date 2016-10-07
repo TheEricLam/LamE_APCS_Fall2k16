dd1f3357801a81920af819c482db924ae62475a8
TheEricLam <18lame45@millshighsch.org>
2016-10-07 07:36:03 -0700

    ,

--- Access Calculate/src/DoMath.java
+++ Access Calculate/src/DoMath.java
@@ -4,8 +4,7 @@ public class DoMath {
 	public static void main(String[] args) {
 		
 		System.out.println(Calculate.square(5));
-		System.out.println(Calculate.discriminant(1, 3, 4));
-		System.out.println(Calculate.quadForm(4, 4, 4));
+		
 	}
 
 }

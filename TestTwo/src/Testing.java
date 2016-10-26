06ff11d020b86c7a49bf32e2c24ae75d570005da
TheEricLam <18lame45@millshighsch.org>
2016-10-26 10:33:15 -0700

    PLEASE COMMIT

---
+++ TestTwo/src/Testing.java
@@ -0,0 +1,14 @@
+
+public class Testing {
+
+	public static void Splitter(String[] args) {
+		String string = "Hello-there";
+		String [] parts = string.split("-");
+		String part1 = parts[0];
+		String part2 = parts[1];
+		System.out.println(part1);
+		System.out.println(part2);
+
+	}
+
+}

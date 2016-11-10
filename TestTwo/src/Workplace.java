a311808a9648b6d46fce0f7590135ea8210f9279
TheEricLam <18lame45@millshighsch.org>
2016-11-10 07:20:31 -0800

    Nov 10

--- TestTwo/src/Workplace.java
+++ TestTwo/src/Workplace.java
@@ -1,55 +1,22 @@
 
 
-public class ArraysLab3 {
-
-
-	public static void main(String[] args) {
-		int[] test1 = {1 ,2 ,3 ,4};
-		int[] test2 = {1, 2, 3, 4};
-		
-		sum(test1, test2);
-		append(test1, 2);
-		remove(test1, 1);
-		
-		
-
-
+class DrumKit {
+	boolean topHap = true;
+	boolean snare = true;
+	
+	void playSnare() {
+		System.out.println("bang bang ba-bang");
 	}
 	
-	//Simple a + b = c algorithem but using a for loop to add up the "i"s
-	public static int [] sum(int[] arr1, int[] arr2){
-		int[] total = new int[arr1.length];   //since both arr1 and arr2 are equal length, i initalized it for one
-		for(int i = 0; i < arr1.length; i++){ 
-			total[i] = arr1[i] + arr2 [i];    // total = arr1 + arr2
-		}
-		return(total);
+	void playTopHat() {
+		System.out.println("ding ding da-ding");
 	}
-	
-	public static int[] append(int[] arr, int num){
-		//int [] total = new int[arr.length];
-		//for(int i = 0; i < arr.length; i++)
-		arr[arr.length + 1] = num;
-		return arr;
-	}
-
-
-	//public static int[] remove(int[] arr, int idx){
-		
-	//}
-	
-	public static int sumEven(int[] arr){
-		int total = 0;
-		for(int i = 0; i < arr.length; i++){
-			if(i%2 == 0){
-				total += i;
-			}
-		}
-		return(total);
-	}
-	
-	
 }
 
-
-
-
+class Workplace {
+	public static void main(String[] args) {
+		DrumKit d = new DrumKit();
+		d.playSnare();
+		d.playTopHat();
+	}
+}

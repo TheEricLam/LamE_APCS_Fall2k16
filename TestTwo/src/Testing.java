260ec3002b3af589128cbd2adceff0d896a1f9e4
TheEricLam <18lame45@millshighsch.org>
2016-11-22 06:33:31 -0800

    nov 22

--- TestTwo/src/Testing.java
+++ TestTwo/src/Testing.java
@@ -1,23 +1,27 @@
+import java.util.*;
 
 public class Testing {
-
-	public static void Splitter(String[] args) {
-		String s = "apples pineapples bread lettus tomato bacon mayo ham bread cheese";
-		String [] parts = s.split(" ");
-		
-		int bread =0;
-		for(int i = 0; i < s.length(); i++){
-			if(parts[i] == "bread"){
-				bread = i;
-			}
+	public static void main (String[] args){
+		Scanner word = new Scanner(System.in);
+		System.out.print("What is your phrase? ");
+		String phrase = word.nextLine();
+		System.out.print("How many times should I repeat it? ");
+		int times = word.nextInt();
+		for (int i = 1; i <= times; i++) {
+		    System.out.println(phrase);
 		}
-		
-		
-	
-		//if(!(parts[0]== "bread")){
-		//	System.out.println("This is not a sandwitch");
-		//}
-		
+		word.close();
+	}
+	public void processName(Scanner console) {
+	    System.out.print("Please enter your full name: ");
+	    String name = console.nextLine();
+	    String[] parts = name.split("\\s+");
+	    System.out.println("Your name in reverse order is " + parts[1] + ", " +
+	        parts[0]);
+	    
+	    console.close();
 	}
 
+	
+	
 }

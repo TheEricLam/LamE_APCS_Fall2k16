8b6fd08b3060cd37cfcffa9ec0b4d90737c6182e
TheEricLam <18lame45@millshighsch.org>
2017-01-16 10:20:33 -0800

    1/16/17

--- TestTwo/src/Workplace.java
+++ TestTwo/src/Workplace.java
@@ -1,27 +1,7 @@
 public class Workplace{
 	public static void main(String[] args){
-		int numOne = 4;
-		int numTwo = 16;
-		
-		System.out.println(gcf(numOne,numTwo));
+	
+		System.out.println("Bigal is a begal");
 	}
-	public static int gcf(int num1, int num2){
-		int greatestCommonFactor = 1;
-		for(int i=1; i<=num1; i++){
-			if(isDivisibleBy(num1,i) && isDivisibleBy(num2,i)){
-				greatestCommonFactor=i;
-			}
-		}
-		return (greatestCommonFactor);
-	}
-	public static boolean isDivisibleBy(int num1, int num2){
-		if(num2==0){
-			throw new IllegalArgumentException("Cannot divide by zero");
-		}
-		if(num1%num2 == 0){
-			return(true);
-		}else{
-			return(false);
-		}
-	}
+	
 }
